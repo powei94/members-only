cdc7a110a28731af5c279d1cbebbc186c6e59c55
Cristobal <cristobal.becerraf@usach.cl>
2020-05-02 16:14:59 -0400

    vistas mejoradas y permisos actualizados

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :require_permission, only: [:edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -72,5 +73,11 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :content)
+    end
+
+    def require_permission
+      if current_user != Post.find(params[:id]).user
+        redirect_to root_path, notice: 'No puedes modificar este Post'
+      end
     end
 end
